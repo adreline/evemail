1c1e03124fac50504fef46e520e39ddc33fbe729
Madeline Niećko <maddie_madlad@mailbox.org>
2023-04-29 12:59:56 +0200

    docs

--- esi/eve-esi/src/sso.js
+++ esi/eve-esi/src/sso.js
@@ -9,7 +9,9 @@ const request = axios.create({
         'Host': 'login.eveonline.com'
     }
 });
-
+/**
+ * App permissions scopes 
+ * @type {*} */
 const scope = [
     'esi-mail.organize_mail.v1',
     'esi-mail.read_mail.v1',
@@ -18,20 +20,41 @@ const scope = [
     'esi-characters.read_corporation_roles.v1',
     'esi-corporations.track_members.v1'
 ];
+/** 
+ * callback path, defined in the app at eve developer portal 
+ * @type {*} */
 const sso_callback = 'https://localhost/eveauth/callback';
-const settings = require('../../../env.json');
+/** 
+ * Will contain app id and app secret in b64 sha 256 format
+ * @type {*} */
+let settings = {};
 const base = 'https://login.eveonline.com/v2/oauth/authorize/';
 
 const sso_response_target = 'v2/oauth/token';
-
+/**
+ * Generates sso OAuth link
+ *
+ * @return {*} 
+ */
 function getSSOLink() {
     return encodeURI(`${base}?response_type=code&redirect_uri=${sso_callback}&client_id=${settings.app.id}&scope=${scope.join(' ')}&state=hehe`);
 }
+/**
+ * Captures callback GET request from ESI OAuth 
+ *
+ * @param {*} url
+ * @return {*} 
+ */
 function isSSORedirect(url) {
     let ex = /^https\:\/\/localhost\/eveauth\/callback\?code\=[\d\w_-]+\&state\=hehe/m;
     return ex.test(url)
 }
-
+/**
+ * Executes the last step at ESI OAuth and returns the Promise of a final auth token
+ *
+ * @param {*} code
+ * @return {*} 
+ */
 function sendSSOAuthRequest(code) {
     const params = new URLSearchParams();
     params.append('grant_type', 'authorization_code');
@@ -41,7 +64,13 @@ function sendSSOAuthRequest(code) {
 
     return request.post(sso_response_target, params);
 }
-
+/**
+ * Listens to web requests the app makes until it detects the ESI callback route
+ * Returns Promise of verification code
+ *
+ * @param {*} session
+ * @return {*} 
+ */
 function getSSOVerificationCode(session) {
     return new Promise((resolve, reject) => {
         session.webRequest.onBeforeRequest((details, callback) => {
@@ -55,8 +84,15 @@ function getSSOVerificationCode(session) {
         })
     })
 }
-
-function showSSOLogin(session) {
+/**
+ * Displays the SSO login page and returns a Promise of SSO token
+ *
+ * @param {*} session
+ * @param {*} env
+ * @return {*} 
+ */
+function showSSOLogin(session, env) {
+    settings = env
     return new Promise((resolve, reject) => {
         const win = new BrowserWindow({})
         win.loadURL(getSSOLink())
