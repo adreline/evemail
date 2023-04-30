074f417b803b3d1e284ca553d399266510cbc03c
Madeline Niećko <maddie_madlad@mailbox.org>
2023-04-30 18:51:16 +0200

    moved most init logic to their own modules

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const { app, BrowserWindow, session, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const { promises: Fs } = require('fs');
 const path = require('path');
 
-const esi = require('./esi/eve-esi');
 const { getEnv } = require('./controller/AskEnv/promiseAskEnv.js');
-const { loginWithSSO } = require('./controller/AskSSO/promiseSSO.js');
+const { getSSO } = require('./sso.js');
 const { getCharacter, getCharacterPicture } = require('./controller/GetDashboard/promiseProfile.js');
 let sso = {}
 let env = {}
+
 
 /**
  * Initializes necessary environment variables such as app id and sso auth token
@@ -15,69 +15,35 @@ let env = {}
  * @return {*} 
  */
 function init(){
-  console.log('init start');
+  console.log('[index.js] init start');
   return new Promise((resolve, reject) => {
-      Fs.access('./env.json')
-      .then(()=>{
-        console.log('env.json detected, reading');
-        return Fs.readFile('./env.json', { encoding: 'utf8' })
-      })
-      .catch(()=>{
-        console.log('env.json not found, asking for input');
-        return getEnv()
-      })
-      .then(data => {
-        console.log('received env.json');
-        if(typeof data === 'string'){
-          console.log('received env via file');
-          env = JSON.parse(data)
-        }else{
-          console.log('received env via user input');
-          env.app = data
-          Fs.writeFile('./env.json', JSON.stringify(env))
-        }
-        console.log('checking for sso token');
-        return Fs.access('./token.json')
-      })
-      .then(()=>{
-        console.log('sso token present, reading');
-        return Fs.readFile('./token.json', { encoding: 'utf8' })
-      })
-      .catch(() => {
-        console.log('sso token not found, asking for login');
-        return loginWithSSO(session, env)
-      })
-      .then(data => {
-        console.log('received sso token');
-        if(typeof data === 'string'){
-          console.log('received token via file');
-          sso = JSON.parse(data)
-        }else{
-          console.log('received token via sso login');
-          sso = data
+    getEnv()
+    .then( settings => {
+      env = settings
+      return getSSO()
+    })
+    .then( token => {
+      sso = token
+      if(!sso.hasOwnProperty('character')){
+        console.log('[index.js] character data not found, pulling');
+        getCharacter(sso.access_token)
+        .then(character => {
+          sso.character = {
+            id: character.characterID,
+            name: character.characterName
+          }
+          return getCharacterPicture(sso.character.id)
+        })
+        .then(filename => {
+          sso.character.picture = filename
           Fs.writeFile('./token.json', JSON.stringify(sso))
-        }
-
-        if(!sso.hasOwnProperty('character')){
-          console.log('character data not found, pulling');
-          getCharacter(sso.access_token)
-          .then(character => {
-            sso.character = {
-              id: character.characterID,
-              name: character.characterName
-            }
-            return getCharacterPicture(sso.character.id)
-          })
-          .then(filename => {
-            sso.character.picture = filename
-            Fs.writeFile('./token.json', JSON.stringify(sso))
-            console.log('pull complete');
-            resolve()
-          })
-        }else{
+          console.log('[index.js] pull complete');
           resolve()
-        }
-      })
+        })
+      }else{
+        resolve()
+      }
+    })
   })
 }
 
@@ -92,7 +58,7 @@ app.whenReady().then(() => {
 
   init()
   .then(()=>{
-    console.log('init finished');
+    console.log('[index.js] init finished');
     ipcMain.handle('getCharacter', () => { return sso.character })
     return dashboard.loadFile('views/index.html');
   })
