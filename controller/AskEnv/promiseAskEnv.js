3f7e12adb269d9184ea5c1bdb5d68fc91bcfbb36
Madeline Niećko <maddie_madlad@mailbox.org>
2023-04-30 18:52:00 +0200

    obscured some of the logic to expose only the data behind it

--- controller/AskEnv/promiseAskEnv.js
+++ controller/AskEnv/promiseAskEnv.js
@@ -1,23 +1,66 @@
 const { ipcMain, BrowserWindow } = require('electron')
 const path = require('path')
+const { promises: Fs } = require('fs');
+const { createHash } = require('crypto');
+
+
 /**
- * Returns a Promise of app id and app secret, taken from user input
+ * Asks the user to fill out env information
+ *
+ * @return {*} 
+ */
+function askForEnv(){
+  return new Promise((resolve, reject) => {
+      const askEnv = new BrowserWindow({ 
+        webPreferences: {
+          preload: path.join(__dirname, 'preloadAskEnv.js'),
+        }
+      })
+      ipcMain.on('getEnv', (event, data) => {
+        resolve(data) 
+        askEnv.close()
+      })
+      askEnv.loadFile('views/windows/_askEnv.html')
+  })
+}
+
+function encryptAppSecret(secret){
+  return Buffer.from(createHash('sha256').update(secret).digest('hex')).toString('base64').replace("==", "");
+}
+
+/**
+ * Returns a Promise of app id and app secret, taken from user input or read from file
  *
  * @return {*} 
  */
 function getEnv(){
     return new Promise((resolve, reject) => {
-          const askEnv = new BrowserWindow({ 
-            webPreferences: {
-              preload: path.join(__dirname, 'preloadAskEnv.js'),
-            }
-          })
-        ipcMain.on('getEnv', (event, data) => {
-            resolve(data) 
-            askEnv.close()
+        Fs.access('./env.json')
+        .then(()=>{
+          console.log('[promiseAskEnv.js] env.json detected, reading');
+          return Fs.readFile('./env.json', { encoding: 'utf8' })
         })
-        askEnv.loadFile('views/windows/_askEnv.html')
-
+        .catch(()=>{
+          console.log('[promiseAskEnv.js] env.json not found, asking for input');
+          return askForEnv()
+        })
+        .then(data => {
+          console.log('[promiseAskEnv.js] received env.json');
+          if(typeof data === 'string'){
+            console.log('[promiseAskEnv.js] received env via file');
+            resolve(JSON.parse(data))
+          }else{
+            console.log('[promiseAskEnv.js] received env via user input');
+            env = {
+              app: {
+                b64_sha_secret: encryptAppSecret(data.secret),
+                id: data.id
+              }
+            };
+            resolve(env)
+            Fs.writeFile('./env.json', JSON.stringify(env))
+          }
+        })
     })
 }
 
