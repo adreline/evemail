ae823a992cc63a9ff17eb5d087621853b69ab060
Madeline Niećko <maddie_madlad@mailbox.org>
2023-05-06 15:47:17 +0200

    Update preloadTemplatesList.js

--- controller/Templates/List/preloadTemplatesList.js
+++ controller/Templates/List/preloadTemplatesList.js
@@ -2,7 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('templates', {
     getTemplates: () => ipcRenderer.invoke('getTemplates'),
-    openTemplateEditor: (template) => ipcRenderer.send('openTemplateEditor', template),
-    deleteTemplate: (template) => ipcRenderer.send('deleteTemplate', template),
-    viewTemplate: (template) => ipcRenderer.send('viewTemplate', template)
+    openTemplateEditor: (id) => ipcRenderer.send('openTemplateEditor', id),
+    reloadTemplatesList: () => ipcRenderer.invoke('reloadTemplatesList'),
+    deleteTemplate: (id) => ipcRenderer.send('deleteTemplate', id),
+    viewTemplate: (id) => ipcRenderer.invoke('previewTemplate', [ id ])
 })
